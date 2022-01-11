81b6ac775caaa6ce18d0e9e11b5ddba7123d7aac
Nate Sheridan <83609521+natesheridan@users.noreply.github.com>
2022-01-11 09:57:06 -0700

    Add API function import to top level componenent

--- src/App.js
+++ src/App.js
@@ -1,10 +1,24 @@
 
 import './App.css';
+import getTopStories from './util/api'
+import React, {useEffect, useState} from 'react'
 
 function App() {
+  const [topStories, setTopStories] = useState([])
+
+  
+  useEffect(()=> {
+    onAppLoad()
+  }, [])
+
+  const onAppLoad = async () => {
+    let nytResponse = await getTopStories("home")
+    setTopStories(nytResponse.results)
+  }
+
+
   return (
     <>
-    
     </>
   )
 }
